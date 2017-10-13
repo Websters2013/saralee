1381dbca65c0deadcf10d5dfe9409f6d0046851b
max_tkhorenko <max.tkh.ua@gmial.com>
2017-10-13 10:47:43 +0300

    recipes

--- app/js/jquery.index.js
+++ app/js/jquery.index.js
@@ -990,8 +990,6 @@
             _rateFrame = _obj.find( '.FSR_container' ),
             _rateItem = _rateFrame.find( '.FSR_full_voting_star' );
 
-        console.log( _rateItem.length() )
-
         //private methods
         var _onEvent = function() {
 
